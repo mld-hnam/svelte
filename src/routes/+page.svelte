eefb79417ee9a8f7fe8bb99c25c7a26d0317b37b
Hnam <knight@nexondv.com>
2023-10-18 15:46:13 +0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let data;
+  $: user = data?.name.first + " " + data?.name.last;
+</script>
+
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+{#if user}
+  <h1>Name: {user}</h1>
+{:else}
+  <h1>error getting user</h1>
+{/if}
